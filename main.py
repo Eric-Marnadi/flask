5a7cc16ddc5731f1460e7bca1a4f54bce6245fdf
Eric Marnadi <ericmarnadi@outlook.com>
2022-12-07 09:46:51 +0000

    Accept requests for link

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, redirect
+from urllib import request
 import os
 
 app = Flask(__name__)
@@ -6,8 +7,21 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    f = open("link.txt", "r")
+    link = f.read()
+    f.close()
+    return redirect(link)
 
-
+@app.route('/change_link', methods=['POST'])
+def change_link():
+    args = request.args
+    data = request.form
+    token = args.get('token')
+    if token == os.environ.get("SECURITY_TOKEN"):
+        link = data.get('link')
+        f = open("link.txt", "w")
+        f.write(link)
+        f.close()
+    
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
